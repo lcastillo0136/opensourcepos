d9bd8776f6d18a1fa6800c6742c303a7551a5463
Luis Castillo <luis.castillo@naranya.com>
2016-02-23 13:44:26 -0600

    Clear all

--- application/language/de-CH/login_lang.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$lang["login_go"] = "Start";
-$lang["login_invalid_username_and_password"] = "Ungültiger Benutzername/Passwort";
-$lang["login_login"] = "Login";
-$lang["login_password"] = "Passwort";
-$lang["login_username"] = "Benutzername";
-$lang["login_welcome_message"] = "Willkommen bei Open Source Point Of Sales. Zum Starten bitte auf ein Modul klicken";
